dabefdf318486c1e137441e4eb1ad8d29cfb67ec
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-13 04:57:52 +0700

    Update main.cpp

--- sarnax.cfx/main.cpp
+++ sarnax.cfx/main.cpp
@@ -180,21 +180,24 @@ void CleanupDeviceD3D()
 
 }
 
-void ResetDevice()
+void CConsole::Clear()
 {
-    ImGui_ImplDX9_InvalidateDeviceObjects();
-    HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
-    if (hr == D3DERR_INVALIDCALL)
-        IM_ASSERT(0);
-    ImGui_ImplDX9_CreateDeviceObjects();
+    COORD topLeft = { 0, 0 };
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen;
+    DWORD written;
+
+    GetConsoleScreenBufferInfo(console, &screen);
+    FillConsoleOutputCharacterA(console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
+    FillConsoleOutputAttribute(console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE, screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
+    SetConsoleCursorPosition(console, topLeft);
+    return;
 }
 
-Settings* Settings::GetInstance()
+void CConsole::SetTitle(_In_ LPCSTR lpConsoleTitle)
 {
-	if (!m_pInstance)
-		m_pInstance = new Settings();
-
-	return m_pInstance;
+    SetConsoleTitleA(lpConsoleTitle);
+    return;
 }
 
 void killdbg()
