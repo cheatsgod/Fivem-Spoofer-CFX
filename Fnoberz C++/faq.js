6329293bd2b5defc80b5b96cefd31d4b8271d7ef
Fnoberz <FnoberzOfficial@pm.me>
2022-10-14 06:18:17 +0700

    Add files via upload

---
+++ Fnoberz C++/faq.js
@@ -0,0 +1,13 @@
+{
+    "content": null,
+    "embeds": [
+      {
+        "description": "**__Question & Answer__:**\n\nPlease read our Q&A  for you to understand more about it.\n\n**__A__:** Will I receive the product after payment\nB: It takes 1-2 days to create a project.\n\n**__A__:** No products ready to ship?\nB: The product will be created after payment for security.\n\n**__A__:** Does the product have a warranty\nB: You can read it here <#986706169781436426>\n\n**__A__:** A: When updating, do I have to pay for it?\nB: update free permanent\n\n**__A__:** Why not sell daily / week / month\nB: we have security reasons that cannot be disclosed\n\n**__A__:**  IF I want to be a reseller, what should I do\nB: read here : <#986855178412109824>\n\n**__A__:** There is no trial version before purchase\nB: as I said, this is a personal project. We built just for you So it doesn't have a trial version and to prevent detection \n\n\n**__A__:** Want to see reviews Where can I find videos\nB: dm <@943374631644045363> to see videos customer reviews.\n\n**__A__:** If I want to play more than 1 person\nB: you can share project with friends +30$/1 person\n maximum 5 person\n\n**__A__:** What do you use to lock products \nB: hardware ids / a generic spoofer can't change it.\n\n**__A__:** Can I upgrade to a reseller if I buy a personal project\nB: sure , can do it \n\n**__A__:** Why are you sure it's safe?\nB: we have more than 10 bypass drivers and development team.\n\n**__Read here !__**\n- If you have additional questions or concerns, **__DM__:** <@943374631644045363>\n- https://github.com/Cloud-Official/Product",
+        "color": 10383,
+        "thumbnail": {
+          "url": "https://cdn.discordapp.com/attachments/986721372451504179/1028056838979207239/Fitness_for_anyone.gif"
+        }
+      }
+    ],
+    "attachments": []
+  }
