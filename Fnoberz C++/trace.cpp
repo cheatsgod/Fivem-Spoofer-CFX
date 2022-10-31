817b3bccba1f03c17ec8bc0fa2c018fe074b2811
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-01 00:12:22 +0700

    Update trace.cpp

--- Fnoberz C++/trace.cpp
+++ Fnoberz C++/trace.cpp
@@ -1,5 +1,26 @@
 #include "trace.hpp"
 
+inline int UseFuntion(int x, int y, int z, int r, float ks)
+{
+	ks /= 2;
+	for (int i = 2; i < 8; i++)
+	{
+		z += 456;
+		r -= 55;
+		x++;
+		y--;
+
+	}
+	for (int i = 0; i < 2; i++)
+	{
+		x++;
+		y += 234;
+		z -= 23;
+		r += 634;
+	}
+	return x + z + r + y;
+}
+
 void trace::setup()
 {
 	// Locate Appdata
